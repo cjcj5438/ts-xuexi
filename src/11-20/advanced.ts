d37cbbcce4e46cf87aa99ceaafbf2f0d49fb123c
chenjing <1002931123@qq.com>
2020-11-30 21:35:01 +0800

    ts 代码调试

--- src/11-20/advanced.ts
+++ src/11-20/advanced.ts
@@ -4,7 +4,6 @@
  * ? 上下文推断 是从左到右的
  */
 let b = [1, null]
-
 // 类型断言
 interface Foo15 {
   bar: number
@@ -13,6 +12,7 @@ interface Foo15 {
 // foo15.bar = 1
 // 其实比较好的是在声明的时候，就声明
 let foo15: Foo15 = {
-  bar: 1
+  bar: 13212456787
 }
+console.log(foo15.bar)
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import  './01-10/datatype'
-import  './01-10/function'
-let hello:string = 'hello ts'
-
-document.querySelectorAll('.app')[0].innerHTML=hello
+// import './01-10/datatype'
+// import './01-10/function'
+import './11-20/advanced'
+let hello: string = 'hello ts'
+document.querySelectorAll('.app')[0].innerHTML = hello
 
