83f5b9c479fc1aef6ae6c0f8fe004ae6f3a0b29a
chenjing <1002931123@qq.com>
2020-11-30 09:14:55 +0800

    泛型基础

---
+++ src/11-20/advanced.ts
@@ -0,0 +1,18 @@
+/**
+ * 泛型
+ * ? 类型推断都是 右向 左的推断
+ * ? 上下文推断 是从左到右的
+ */
+let b = [1, null]
+
+// 类型断言
+interface Foo15 {
+  bar: number
+}
+// let foo15 = {} as Foo15
+// foo15.bar = 1
+// 其实比较好的是在声明的时候，就声明
+let foo15: Foo15 = {
+  bar: 1
+}
+
--- src/11-20/class.ts
+++ src/11-20/class.ts
@@ -1,11 +1,11 @@
 // 抽象类 ， 不能被创建实例  只能被继承,
 // 抽象类中定义方法。那么在子类中就不用再实现了。 实现了方法的复用
-abstract class Animal { 
-  eat(){
+abstract class Animal {
+  eat() {
     console.log('eat')
   }
   // 再抽象类中定义抽象方法，不具体实现。这样就构成了抽象方法
-  abstract sleep():void; // 在子类中想多态的写法
+  abstract sleep(): void; // 在子类中想多态的写法
 }
 // let animal = new Animal()
 
@@ -15,14 +15,15 @@ class Dog11 extends Animal {
     this.name = name;
   }
   public name: string; // name属性在实例上不在原型上，且必须有初始值
-  run() { } // 这在原型上
-  sleep(){
+  run() {} // 这在原型上
+
+  sleep() {
     console.log('dog sleep')
   }
 }
-class Cat11 extends Animal {
+        class Cat11 extends Animal {
   // 继承抽象类的时候可以 不用写 constructor  ？
-  sleep(){
+  sleep() {
     console.log('cat sleep')
   }
 }
@@ -40,20 +41,54 @@ cat11.sleep()
 
 // 类的成员方法可以返回一个 this 可以很方便的实现链式调用
 
-class WorkFlow11{
-  step1(){
+class WorkFlow11 {
+  step1() {
     return this
   }
-  step2(){
+  step2() {
     return this
   }
 }
 // 这样就实现的 方法的链式调用
 new WorkFlow11().step1().step2()
-class Myflow extends WorkFlow11{
-  next(){
+class Myflow extends WorkFlow11 {
+  next() {
     return this;
   }
 }
 // 父类和子类之间 调用
 new Myflow().next().step1().next().step2()
+
+let adsf = {
+  "arrow": [1, 1], // The same as above.
+  // The second number is how much space to the right, can be negative.
+  "assignment": [1, 1], // The same as above.
+  "comment": 2, // Special how much space to add between the trailing comment and the code.
+  "colon": [0, 1], //The first number specify how much space to add to the left, can be negative.
+}
+
+// ! 红色的高亮注释
+// ? 蓝色的高亮注释
+// * 绿色的高亮注释
+// todo 橙色的高亮注释
+// // 灰色带删除线的注释
+// 普通的注释
+
+
+/**
+  // ! 红色的高亮注释
+  // ? 蓝色的高亮注释
+  // * 绿色的高亮注释
+  // todo 橙色的高亮注释
+  // // 灰色带删除线的注释
+*/
+
+{
+  {
+    {
+      {
+        {}
+      }
+    }
+  }
+}
